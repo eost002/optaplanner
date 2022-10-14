16746ce8b1782bdc5887bdffb520007b552caf74
Eric Oon <d1329479@ncsdevnet.com>
2022-10-14 19:56:06 +0800

    first commit

---
+++ use-cases/maintenance-scheduling/src/main/java/org/acme/maintenancescheduling/bootstrap/DemoDataGenerator.java
@@ -0,0 +1,152 @@
+package org.acme.maintenancescheduling.bootstrap;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.chrono.IsoChronology;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import org.acme.maintenancescheduling.domain.Crew;
+import org.acme.maintenancescheduling.domain.Job;
+import org.acme.maintenancescheduling.domain.WorkCalendar;
+import org.acme.maintenancescheduling.persistence.CrewRepository;
+import org.acme.maintenancescheduling.persistence.JobRepository;
+import org.acme.maintenancescheduling.persistence.WorkCalendarRepository;
+import org.acme.maintenancescheduling.solver.EndDateUpdatingVariableListener;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import io.quarkus.runtime.StartupEvent;
+
+@ApplicationScoped
+public class DemoDataGenerator {
+
+    @ConfigProperty(name = "schedule.demoData", defaultValue = "SMALL")
+    public DemoData demoData;
+
+    public enum DemoData {
+        NONE,
+        SMALL,
+        LARGE
+    }
+
+    @Inject
+    WorkCalendarRepository workCalendarRepository;
+    @Inject
+    CrewRepository crewRepository;
+    @Inject
+    JobRepository jobRepository;
+
+    @Transactional
+    public void generateDemoData(@Observes StartupEvent startupEvent) {
+        if (demoData == DemoData.NONE) {
+            return;
+        }
+
+        List<Crew> crewList = new ArrayList<>();
+        crewList.add(new Crew("T1"));
+        crewList.add(new Crew("T3"));
+        crewList.add(new Crew("T6"));
+        crewList.add(new Crew("T8"));
+        crewList.add(new Crew("T2"));
+        crewList.add(new Crew("T4"));
+        crewList.add(new Crew("T5"));
+        crewList.add(new Crew("T7"));
+        if (demoData == DemoData.LARGE) {
+            crewList.add(new Crew("Delta crew"));
+            crewList.add(new Crew("Epsilon crew"));
+        }
+        crewRepository.persist(crewList);
+
+        LocalDate fromDate = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+        int weekListSize = 8;
+        LocalDate toDate = fromDate.plusWeeks(weekListSize);
+        workCalendarRepository.persist(new WorkCalendar(fromDate, toDate));
+
+        final String[] JOB_AREA_NAMES = {
+                "Shift A", "Shift B", "Shift C", "Day Off", "Weekend Shift"};
+        
+
+        List<Job> jobList = new ArrayList<>();
+
+        int jobListSize = 448; // 56 = One week of jobs
+        int case2Counter = 0;
+        int case4Counter = 0;
+        int case5Counter = 0;
+        int case6Counter = 0;
+        for (int i = 0; i < jobListSize; i++) {
+            int noOfWeeksCompleted = i / 56;
+            int dayOffsetInWeek = (i % 56) / 8; // 0 = Monday
+            if (i % 56 == 0) {
+                case2Counter = 0;
+                case4Counter = 0;
+                case5Counter = 0;
+                case6Counter = 0;
+            }
+            int dayOffsetFromInitial = (noOfWeeksCompleted * 7) + dayOffsetInWeek;
+            int weekNo = dayOffsetFromInitial / 7;
+            switch (dayOffsetInWeek) {
+                case 0:
+                case 1:
+                case 3:
+                    String jobArea = JOB_AREA_NAMES[i % 2];
+                    LocalDate readyDate = EndDateUpdatingVariableListener.calculateEndDate(fromDate, dayOffsetFromInitial);
+                    LocalDate dueDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    LocalDate idealEndDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    Set<String> tagSet = Set.of(jobArea);
+                    jobList.add(new Job(jobArea, 1, readyDate, dueDate, idealEndDate, tagSet, weekNo));
+                    break;
+                case 2:
+                    jobArea = JOB_AREA_NAMES[case2Counter < 4 ? 0 : (case2Counter < 6 ? 1 : 3)];
+                    case2Counter++;
+                    readyDate = EndDateUpdatingVariableListener.calculateEndDate(fromDate, dayOffsetFromInitial);
+                    dueDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    idealEndDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    tagSet = Set.of(jobArea);
+                    jobList.add(new Job(jobArea, 1, readyDate, dueDate, idealEndDate, tagSet, weekNo));
+                    break;
+                case 4:
+                    jobArea = JOB_AREA_NAMES[case4Counter < 3 ? 1 : (case4Counter < 5 ? 0 : (case4Counter < 7 ? 3 : 2))];
+                    case4Counter++;
+                    readyDate = EndDateUpdatingVariableListener.calculateEndDate(fromDate, dayOffsetFromInitial);
+                    dueDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    idealEndDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    tagSet = Set.of(jobArea);
+                    jobList.add(new Job(jobArea, 1, readyDate, dueDate, idealEndDate, tagSet, weekNo));
+                    break;
+                case 5:
+                    if (case5Counter > 1)
+                        break;
+                    case5Counter++;
+                    jobArea = JOB_AREA_NAMES[4];
+                    readyDate = EndDateUpdatingVariableListener.calculateEndDate(fromDate, dayOffsetFromInitial);
+                    dueDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    idealEndDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    tagSet = Set.of(jobArea);
+                    jobList.add(new Job(jobArea, 1, readyDate, dueDate, idealEndDate, tagSet, weekNo));
+                    break;
+                case 6:
+                    if (case6Counter > 1)
+                        break;
+                    case6Counter++;
+                    jobArea = JOB_AREA_NAMES[4];
+                    readyDate = EndDateUpdatingVariableListener.calculateEndDate(fromDate, dayOffsetFromInitial);
+                    dueDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    idealEndDate = EndDateUpdatingVariableListener.calculateEndDate(readyDate, 1);
+                    tagSet = Set.of(jobArea);
+                    jobList.add(new Job(jobArea, 1, readyDate, dueDate, idealEndDate, tagSet, weekNo));
+                    break;
+            }
+            
+        }
+        
+        jobRepository.persist(jobList);
+    }
+
+}
---
+++ use-cases/maintenance-scheduling/src/main/java/org/acme/maintenancescheduling/solver/MaintenanceScheduleConstraintProvider.java
@@ -0,0 +1,273 @@
+package org.acme.maintenancescheduling.solver;
+
+import java.io.Console;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.GroupLayout.Group;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.sum;
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.sumDuration;
+import static org.optaplanner.core.api.score.stream.Joiners.equal;
+import static org.optaplanner.core.api.score.stream.Joiners.filtering;
+import static org.optaplanner.core.api.score.stream.Joiners.lessThan;
+import static org.optaplanner.core.api.score.stream.Joiners.overlapping;
+
+import org.acme.maintenancescheduling.domain.Job;
+import org.optaplanner.core.api.score.buildin.hardsoftlong.HardSoftLongScore;
+import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.stream.ConstraintCollectors;
+import org.optaplanner.core.api.score.stream.ConstraintFactory;
+import org.optaplanner.core.api.score.stream.ConstraintProvider;
+
+public class MaintenanceScheduleConstraintProvider implements ConstraintProvider {
+
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
+        return new Constraint[] {
+                // Hard constraints
+                crewConflict(constraintFactory),
+                oddWeekTeamInRespectiveShiftA(constraintFactory),
+                oddWeekTeamInRespectiveShiftB(constraintFactory),
+                evenWeekTeamInRespectiveShiftA(constraintFactory),
+                evenWeekTeamInRespectiveShiftB(constraintFactory),
+                
+                wednesday(constraintFactory),
+                friday(constraintFactory),
+                saturday(constraintFactory),
+                sunday(constraintFactory),
+                oneTeamCanAtMostDo1ShiftCInA2MonthPeriod(constraintFactory),
+                oneTeamWedDayOffWillNotReoccurLessThan4Week(constraintFactory),
+
+                readyDate(constraintFactory),
+                dueDate(constraintFactory),
+                // Soft constraints
+                //beforeIdealEndDate(constraintFactory),
+                //afterIdealEndDate(constraintFactory),
+                //tagConflict(constraintFactory),
+                ////
+        };
+    }
+
+    // ************************************************************************
+    // Hard constraints
+    // ************************************************************************
+
+    public Constraint crewConflict(ConstraintFactory constraintFactory) {
+        // A crew can do at most one maintenance job at the same time.
+        return constraintFactory
+                .forEachUniquePair(Job.class,
+                        equal(Job::getCrew),
+                        overlapping(Job::getStartDate, Job::getEndDate))
+                .penalizeLong(HardSoftLongScore.ONE_HARD,
+                        (job1, job2) -> DAYS.between(
+                                job1.getStartDate().isAfter(job2.getStartDate())
+                                        ? job1.getStartDate() : job2.getStartDate(),
+                                job1.getEndDate().isBefore(job2.getEndDate())
+                                        ? job1.getEndDate() : job2.getEndDate()))
+                .asConstraint("Crew conflict");
+    }
+
+    public Constraint oddWeekTeamInRespectiveShiftA(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Job.class)
+                .filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.MONDAY || j.getReadyDate().getDayOfWeek() == DayOfWeek.TUESDAY || j.getReadyDate().getDayOfWeek() == DayOfWeek.THURSDAY)
+                .filter(j -> isOddWeek(j))
+                .filter(job -> (job.getCrew().getName() == "T2"
+                        || job.getCrew().getName() == "T4"
+                        || job.getCrew().getName() == "T5"
+                        || job.getCrew().getName() == "T7")
+                        && job.getName() != "Shift B")
+                .penalize(HardSoftLongScore.ONE_HARD)
+                .asConstraint("ow Wrong Team in Shift B");
+    }
+
+    public Constraint oddWeekTeamInRespectiveShiftB(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Job.class)
+                .filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.MONDAY || j.getReadyDate().getDayOfWeek() == DayOfWeek.TUESDAY || j.getReadyDate().getDayOfWeek() == DayOfWeek.THURSDAY)
+                .filter(j -> isOddWeek(j))
+                .filter(job -> (job.getCrew().getName() == "T1"
+                        || job.getCrew().getName() == "T3"
+                        || job.getCrew().getName() == "T6"
+                        || job.getCrew().getName() == "T8")
+                        && job.getName() != "Shift A" )
+                .penalize(HardSoftLongScore.ONE_HARD)
+                .asConstraint("ow Wrong Team A");
+    }
+
+    public Constraint evenWeekTeamInRespectiveShiftA(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Job.class)
+                .filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.MONDAY || j.getReadyDate().getDayOfWeek() == DayOfWeek.TUESDAY || j.getReadyDate().getDayOfWeek() == DayOfWeek.THURSDAY)
+                .filter(j -> !isOddWeek(j))
+                .filter(job -> (job.getCrew().getName() == "T2"
+                        || job.getCrew().getName() == "T4"
+                        || job.getCrew().getName() == "T5"
+                        || job.getCrew().getName() == "T7")
+                        && job.getName() != "Shift A")
+                .penalize(HardSoftLongScore.ONE_HARD)
+                .asConstraint("ew Wrong Team in Shift B");
+    }
+
+    public Constraint evenWeekTeamInRespectiveShiftB(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Job.class)
+                .filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.MONDAY || j.getReadyDate().getDayOfWeek() == DayOfWeek.TUESDAY || j.getReadyDate().getDayOfWeek() == DayOfWeek.THURSDAY)
+                .filter(j -> !isOddWeek(j))
+                .filter(job -> (job.getCrew().getName() == "T1"
+                        || job.getCrew().getName() == "T3"
+                        || job.getCrew().getName() == "T6"
+                        || job.getCrew().getName() == "T8")
+                        && job.getName() != "Shift B" )
+                .penalize(HardSoftLongScore.ONE_HARD)
+                .asConstraint("ew Wrong Team A");
+    }
+
+    public Constraint wednesday(ConstraintFactory constraintFactory) {
+        var wednesdayJobs = constraintFactory.forEach(Job.class).filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.WEDNESDAY && j.getName() == "Shift A");
+
+        return constraintFactory.forEach(Job.class)
+            .filter(j -> j.getStartDate().getDayOfWeek() == DayOfWeek.TUESDAY && j.getName() == "Shift B")
+            .join(wednesdayJobs, equal(Job::getWeekNo), equal(Job::getCrew))
+            .penalize(HardSoftLongScore.ofSoft(1000000))
+            .asConstraint("wednesday");
+    }
+
+    public Constraint friday(ConstraintFactory constraintFactory) {
+        var dayOffAndShiftAJobs = constraintFactory.forEach(Job.class)
+            .filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.FRIDAY 
+            && (j.getName() == "Shift A" || j.getName() == "Day Off"));
+
+        return constraintFactory.forEach(Job.class)
+            .filter(j -> j.getStartDate().getDayOfWeek() == DayOfWeek.THURSDAY && j.getName() == "Shift B")
+            .join(dayOffAndShiftAJobs, equal(Job::getWeekNo), equal(Job::getCrew))
+            .penalize(HardSoftLongScore.ofSoft(1000000))
+            .asConstraint("friday");
+    }
+
+    public Constraint oneTeamCanAtMostDo1ShiftCInA2MonthPeriod(ConstraintFactory constraintFactory) {
+        var shiftCJobs = constraintFactory.forEach(Job.class).filter(j -> j.getName() == "Shift C");
+        return constraintFactory.forEach(Job.class).filter(j -> j.getName() == "Shift C").join(shiftCJobs)
+            .filter((x, y) -> x.getCrew().getName() == y.getCrew().getName() 
+            && x.getWeekNo() != y.getWeekNo()
+            && Math.abs(x.getWeekNo() - y.getWeekNo()) < 8 )
+            .penalize(HardSoftLongScore.ofSoft(1100000))
+            .asConstraint("oneTeamCanAtMostDo1ShiftCInA2MonthPeriod");
+    }
+
+    public Constraint oneTeamWedDayOffWillNotReoccurLessThan4Week(ConstraintFactory constraintFactory) {
+        var WedDayOffJobs = constraintFactory.forEach(Job.class).filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.WEDNESDAY && j.getName() == "Day Off");
+        return constraintFactory.forEach(Job.class).filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.WEDNESDAY && j.getName() == "Day Off").join(WedDayOffJobs)
+            .filter((x, y) -> x.getCrew().getName() == y.getCrew().getName() 
+            && x.getWeekNo() != y.getWeekNo()
+            && Math.abs(x.getWeekNo() - y.getWeekNo()) < 4 )
+            .penalize(HardSoftLongScore.ofSoft(1100000))
+            .asConstraint("oneTeamWedDayOffWillNotReoccurLessThan4Week");
+    }
+
+    public Constraint saturday(ConstraintFactory constraintFactory) {
+        var saturdayJob = constraintFactory.forEach(Job.class).filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.SATURDAY);
+
+        return constraintFactory.forEach(Job.class)
+            .filter(j -> j.getStartDate().getDayOfWeek() == DayOfWeek.FRIDAY && j.getName() != "Day Off")
+            .join(saturdayJob, equal(Job::getWeekNo), equal(Job::getCrew))
+            .penalize(HardSoftLongScore.ofSoft(900000))
+            .asConstraint("saturday");
+    }
+
+    public Constraint sunday(ConstraintFactory constraintFactory) {
+        var sundayJob = constraintFactory.forEach(Job.class).filter(j -> j.getReadyDate().getDayOfWeek() == DayOfWeek.SUNDAY);
+
+        return constraintFactory.forEach(Job.class)
+            .filter(j -> j.getStartDate().getDayOfWeek() == DayOfWeek.FRIDAY && j.getName() != "Day Off")
+            .join(sundayJob, equal(Job::getWeekNo), equal(Job::getCrew))
+            .penalize(HardSoftLongScore.ofSoft(900000))
+            .asConstraint("sunday");
+    }
+
+    
+
+    public Constraint readyDate(ConstraintFactory constraintFactory) {
+        // Don't start a maintenance job before its ready to start.
+        return constraintFactory.forEach(Job.class)
+                .filter(job -> job.getReadyDate() != null
+                        && job.getStartDate().isBefore(job.getReadyDate()))
+                .penalizeLong(HardSoftLongScore.ONE_HARD,
+                        job -> DAYS.between(job.getStartDate(), job.getReadyDate()))
+                .asConstraint("Ready date");
+    }
+
+    public Constraint dueDate(ConstraintFactory constraintFactory) {
+        // Don't end a maintenance job after its due.
+        return constraintFactory.forEach(Job.class)
+                .filter(job -> job.getDueDate() != null
+                        && job.getEndDate().isAfter(job.getDueDate()))
+                .penalizeLong(HardSoftLongScore.ONE_HARD,
+                        job -> DAYS.between(job.getDueDate(), job.getEndDate()))
+                .asConstraint("Due date");
+    }
+
+
+    // ************************************************************************
+    // Soft constraints
+    // ************************************************************************
+
+    public Constraint beforeIdealEndDate(ConstraintFactory constraintFactory) {
+        // Early maintenance is expensive because the sooner maintenance is done, the sooner it needs to happen again.
+        return constraintFactory.forEach(Job.class)
+                .filter(job -> job.getIdealEndDate() != null
+                        && job.getEndDate().isBefore(job.getIdealEndDate()))
+                .penalizeLong(HardSoftLongScore.ofSoft(1),
+                        job -> DAYS.between(job.getEndDate(), job.getIdealEndDate()))
+                .asConstraint("Before ideal end date");
+    }
+
+    public Constraint afterIdealEndDate(ConstraintFactory constraintFactory) {
+        // Late maintenance is risky because delays can push it over the due date.
+        return constraintFactory.forEach(Job.class)
+                .filter(job -> job.getIdealEndDate() != null
+                        && job.getEndDate().isAfter(job.getIdealEndDate()))
+                .penalizeLong(HardSoftLongScore.ofSoft(1_000_000),
+                        job -> DAYS.between(job.getIdealEndDate(), job.getEndDate()))
+                .asConstraint("After ideal end date");
+    }
+    
+    public Constraint tagConflict(ConstraintFactory constraintFactory) {
+        // Avoid overlapping maintenance jobs with the same tag (for example road maintenance in the same area).
+        return constraintFactory
+                .forEachUniquePair(Job.class,
+                        overlapping(Job::getStartDate, Job::getEndDate),
+                        // TODO Use intersecting() when available https://issues.redhat.com/browse/PLANNER-2558
+                        filtering((job1, job2) -> !Collections.disjoint(
+                                job1.getTagSet(), job2.getTagSet())))
+                .penalizeLong(HardSoftLongScore.ofSoft(1_000),
+                        (job1, job2) -> {
+                            Set<String> intersection = new HashSet<>(job1.getTagSet());
+                            intersection.retainAll(job2.getTagSet());
+                            long overlap = DAYS.between(
+                                    job1.getStartDate().isAfter(job2.getStartDate())
+                                            ? job1.getStartDate()  : job2.getStartDate(),
+                                    job1.getEndDate().isBefore(job2.getEndDate())
+                                            ? job1.getEndDate() : job2.getEndDate());
+                            return intersection.size() * overlap;
+                        })
+                .asConstraint("Tag conflict");
+    }
+
+    public boolean isOddWeek(Job job) {
+        if (job.getWeekNo() % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
